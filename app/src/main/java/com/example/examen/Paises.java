51658c36bf5f2897b1072608516890a882c29c0d
KMOG26031998 <karina.ordonez2016@uteq.edu.ec>
2020-07-23 17:03:18 -0500

    1

---
+++ app/src/main/java/com/example/examen/Paises.java
@@ -0,0 +1,25 @@
+package com.example.examen;
+
+public class Paises {
+    private String name;
+    private String topLevelDomain;
+    private String alpha2Code;
+    private String alpha3Code;
+    private Integer callingCodes;
+    private String capital;
+    private String altSpellingM;
+    private String region;
+    private String subregion;
+    private Integer population;
+    private Integer latng;
+    private String demonym;
+    private Integer area;
+    private Integer gini;
+    private String timesones;
+    private String borders;
+    private String nativeName;
+    private Integer numericCode;
+
+
+
+}
