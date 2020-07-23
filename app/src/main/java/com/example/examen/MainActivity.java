eef1a5ef1be51358df3fef3f3a4e40b73d55d1d3
KMOG26031998 <karina.ordonez2016@uteq.edu.ec>
2020-07-23 18:24:03 -0500

    3

--- app/src/main/java/com/example/examen/MainActivity.java
+++ app/src/main/java/com/example/examen/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.examen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.Placeholder;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+private Placeholder placeholder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       placeholder = findViewById(R.id.placeholder);
+    }
+    public void swapView(View v)
+    {
+        Object transitionManager;
+        transitionManager.beginDelayedTrons(layout);
+
     }
 }
